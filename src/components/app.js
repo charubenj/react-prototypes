9b5a01dec38a85843ba4a7433584d77549104dee
Charu Benjwal <charubenjwal04@gmail.com>
2019-02-10 13:22:53 -0800

    Prototype-functional-component

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,34 @@
+import React from 'react';
+import Table from './table';
+
+
+
+const students=[
+    {
+        name:'Nick',
+        course:'math',
+        grade:'90'
+    },
+    {
+        name:'Jack',
+        course:'math',
+        grade:'80'
+    }
+]
+
+
+
+
+
+const App=()=>{
+    return (
+        <div className='container'>
+            <h1>"Student Grade Table"</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
+
+export default App;
 
 
