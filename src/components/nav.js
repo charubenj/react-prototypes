faf108badbc97474041f7b2dc82a59bf3c0a3981
Charu Benjwal <charubenjwal04@gmail.com>
2019-02-17 20:37:10 -0800

    React Routing with React Router v4

---
+++ src/components/nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './nav.css'
+
+export default props=>{
+
+    return(
+        <ul className= "nav nav-tabs mt-3">
+            <li  className="nav-item">
+                <NavLink exact to ="/" className="nav-link" activeClassName="active selected">Welcome</NavLink>
+            </li>
+            <li  className="nav-item">
+                <NavLink to="/our-macarons" className="nav-link" activeClassName="active selected">Our-Macarons</NavLink>
+            </li>
+            <li  className="nav-item">
+                <NavLink to="/gifts-partiess" className="nav-link" activeClassName="active selected">GiftsParties</NavLink>
+            </li>
+            <li  className="nav-item">
+                <NavLink to="/contact" className="nav-link" activeClassName="active selected">Contact</NavLink>
+            </li>
+
+
+        </ul>
+    )
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default ()=>{
+    return (
+
+        <div>
+            <h1>
+                <p>
+                    lorem ipsum
+                </p>
+            </h1>
+        </div>
+    )
+
+
+}
