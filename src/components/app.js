81f1803bbb43c17ff2edcf15414d08cb766bcd21
Charu Benjwal <charubenjwal04@gmail.com>
2019-02-17 16:27:37 -0800

    Completed prototype React Handling Inputs / Forms

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,49 @@
+import React, {Component} from 'react';
+import ContactList from './contact_list';
+import ContactForm from './contact_form';
+import contactData from '../data/contacts.js';
 
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            contacts: contactData
+        }
+        this.addContact = this.addContact.bind(this);
+    }
+
+    addContact(contact) {
+
+        console.log('App: addComponent:', contact);
+
+        this.setState({
+            contact : [contact,...this.state.contacts]
+        })
+    }
+
+
+
+
+
+render(){
+
+
+    return (
+        <div className='container'>
+            <h1 className='text-center'>AddressBook</h1>
+            <div className="row">
+                <div className="col-4">
+                    <ContactForm add={this.addContact}/>
+                </div>
+                <ContactList contact={this.state.contacts}/>
+
+            </div>
+        </div>
+
+    )
+}
+}
+
+export default App;
 
---
+++ src/data/contacts.js
@@ -0,0 +1,23 @@
+
+
+export default [
+    {
+        firstName: 'nick',
+        lastName :'jonas',
+        phone : 9090909090,
+        email :'abc@ab.com',
+    },
+    {
+        firstName: 'pick',
+        lastName :'jonas',
+        phone : 8080808080,
+        email :'def@ab.com',
+    },
+
+    {
+        firstName: 'chik',
+        lastName :'jonas',
+        phone : 9080808080,
+        email :'xyz@ab.com',
+    }
+];
